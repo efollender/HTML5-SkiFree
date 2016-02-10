e1f7ec1999e6308382051bc542278fe92f06d654
Emma Follender <efollender@gmail.com>
2016-02-10 14:44:38 -0500

    Updated reset function

--- src/actions.js
+++ src/actions.js
@@ -4,135 +4,139 @@ import * as creators from './action_creators';
 
 //util
 const getX = state => {
-	const direction = state.getIn(['game', 'skier', 'position']);
-	switch (direction) {
-		case 'right':
-			return -1;
-			break;
-		case 'left':
-			return 1;
-			break;
-		default:
-			return 0;
-			break;
-	}
-	return 0;
+  const direction = state.getIn(['game', 'skier', 'position']);
+  switch (direction) {
+    case 'right':
+      return -1;
+      break;
+    case 'left':
+      return 1;
+      break;
+    default:
+      return 0;
+      break;
+  }
+  return 0;
 }
 
 export const INITIAL_STATE = Map({
-	game: Map({
-		jumps: List(),
-		monster: Map(),
-		skier: Map({
-			state: 'default',
-			position: 'down',
-			jump: Map({
-				asset: 'player_jump.png',
-				status: 'jump'
-			}),
-			dead: Map({
-				asset: 'player_dead.png',
-				status: 'dead'
-			}),
-			default: Map({
-				asset: 'player_down.png',
-				status: 'default'
-			})
-		}),
-		trees: List(),
-		stats: Map({
-			points: 0,
-			moving: false,
-			altitude: 15000,
-			clock: Date.now()
-		}),
-		settings: Map({
-			gravity: 1
-		})
-	})
+  game: Map({
+    jumps: List(),
+    monster: Map(),
+    skier: Map({
+      state: 'default',
+      position: 'down',
+      jump: Map({
+        asset: 'player_jump.png',
+        status: 'jump'
+      }),
+      dead: Map({
+        asset: 'player_dead.png',
+        status: 'dead'
+      }),
+      default: Map({
+        asset: 'player_down.png',
+        status: 'default'
+      })
+    }),
+    trees: List(),
+    stats: Map({
+      points: 0,
+      moving: false,
+      altitude: 15000,
+      clock: Date.now()
+    }),
+    settings: Map({
+      gravity: 1
+    })
+  })
 });
 
 export function addTree(state, pos) {
-	return state.updateIn(['game','trees'], oldTrees => {
-		return oldTrees.push(fromJS({
-			x: pos.randomX,
-			y: pos.randomY
-		}));
-	});
+  return state.updateIn(['game','trees'], oldTrees => {
+    return oldTrees.push(fromJS({
+      x: pos.randomX,
+      y: pos.randomY
+    }));
+  });
 }
 
 export function addJump(state, pos) {
-	return state.updateIn(['game','jumps'], oldJumps => {
-		return oldJumps.push(fromJS({
-			x: pos.randomX,
-			y: pos.randomY
-		}));
-	});
+  return state.updateIn(['game','jumps'], oldJumps => {
+    return oldJumps.push(fromJS({
+      x: pos.randomX,
+      y: pos.randomY
+    }));
+  });
 }
 
 export function handleJump(state) {
-	return state.setIn(['game', 'skier', 'state'], 'jump');
+  const oldGrav = state.getIn(['game', 'settings', 'gravity']);
+  const newState = state.setIn(['game', 'skier', 'state'], 'jump');
+  return newState.setIn(['game', 'settings', 'gravity'], oldGrav * 2);
 }
 
 export function handleTree(state) {
-	const newState = state.setIn(['game', 'skier', 'state'], 'dead');
-	return newState.setIn(['game', 'stats', 'moving'], false);
+  const newState = state.setIn(['game', 'skier', 'state'], 'dead');
+  return newState.setIn(['game', 'stats', 'moving'], false);
 }
 
 export function moveLeft(state) {
-	return state.updateIn(['game', 'skier', 'position'], oldPosition => {
-		return 'left';
-	});
+  return state.updateIn(['game', 'skier', 'position'], oldPosition => {
+    return 'left';
+  });
 }
 
 export function moveRight(state) {
-	return state.updateIn(['game', 'skier', 'position'], oldPosition => {
-		return 'right';
-	});
+  return state.updateIn(['game', 'skier', 'position'], oldPosition => {
+    return 'right';
+  });
 }
 
 export function moveDown(state) {
-	return state.updateIn(['game', 'skier', 'position'], oldPosition => {
-		return 'down';
-	});
+  return state.updateIn(['game', 'skier', 'position'], oldPosition => {
+    return 'down';
+  });
 }
 
 export function resetSkier(state) {
-	const newState = state.setIn(['game', 'skier', 'state'], 'default');
-	return newState.setIn(['game', 'stats', 'moving'], true);
+  let newState = state.setIn(['game', 'skier', 'state'], 'default');
+  newState = newState.setIn(['game', 'stats', 'moving'], true);
+  newState = newState.setIn(['game', 'settings', 'gravity'], 1);
+  return newState;
 }
 
 export function startGame(state) {
-	return state.updateIn(['game', 'stats'], oldStats => {
-		return oldStats.merge(fromJS({
-			moving: !oldStats.get('moving'),
-			clock: Date.now()
-		}));
-	});
+  return state.updateIn(['game', 'stats'], oldStats => {
+    return oldStats.merge(fromJS({
+      moving: !oldStats.get('moving'),
+      clock: Date.now()
+    }));
+  });
 }
 
 export function updateGravity(state, gravity) {
-	return state.setIn(['game', 'settings', 'gravity'], gravity);
+  return state.setIn(['game', 'settings', 'gravity'], gravity);
 }
 
 export function updateTrees(state, gameSize) {
-	const xFactor = getX(state);
-	const dec = state.getIn(['game', 'settings', 'gravity']);
-	const randomX = size => { return Math.floor(Math.random() * size.x); };
-	const updatePositions = oldState => {
-		return oldState.map(obj => {
-			let updatedY = Math.floor(Math.random() * 100) + gameSize.y;
-			if (dec < 2) updatedY = gameSize.y + 1;
-			let newY = (obj.get('y') < 0) ? updatedY  : obj.get('y') - dec;
-			let newX = (obj.get('y') < 0) ? randomX(gameSize) : obj.get('x') + xFactor;
-			return Map({
-				x: newX,
-				y: newY
-			});
-		});
-	}
-	const trees = state.updateIn(['game', 'trees'], updatePositions);
-	const jumps = trees.updateIn(['game', 'jumps'], updatePositions);
-	const oldAlt = state.getIn(['game', 'stats', 'altitude']);
-	return jumps.setIn(['game', 'stats', 'altitude'], oldAlt - dec);
+  const xFactor = getX(state);
+  const dec = state.getIn(['game', 'settings', 'gravity']);
+  const randomX = size => { return Math.floor(Math.random() * size.x); };
+  const updatePositions = oldState => {
+    return oldState.map(obj => {
+      let updatedY = Math.floor(Math.random() * 100) + gameSize.y;
+      if (dec < 2) updatedY = gameSize.y + 1;
+      let newY = (obj.get('y') < 0) ? updatedY  : obj.get('y') - dec;
+      let newX = (obj.get('y') < 0) ? randomX(gameSize) : obj.get('x') + xFactor;
+      return Map({
+        x: newX,
+        y: newY
+      });
+    });
+  }
+  const trees = state.updateIn(['game', 'trees'], updatePositions);
+  const jumps = trees.updateIn(['game', 'jumps'], updatePositions);
+  const oldAlt = state.getIn(['game', 'stats', 'altitude']);
+  return jumps.setIn(['game', 'stats', 'altitude'], oldAlt - dec);
 }
